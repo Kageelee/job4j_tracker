8e95e068fab5b1c90a32d78542307c0c4dcb03c4
Kageelee <kirill8102@mail.ru>
2026-09-07 23:30:32 +0500

    4.1. Элементы интерфейса [#504948]

--- src/main/java/ru/job4j/polymorphism/TownCar.java
+++ src/main/java/ru/job4j/polymorphism/TownCar.java
@@ -1,9 +1,10 @@
 package ru.job4j.polymorphism;
 
 public class TownCar implements Vehicle {
+
     @Override
     public void changeGear() {
-        System.out.println("Коробка передач автомат. Поставить селектор в режим D.");
+        System.out.println("Коробка автомат. Поставить селектор в режим D.");
     }
 
     @Override
@@ -24,5 +25,10 @@ public class TownCar implements Vehicle {
     @Override
     public void refill() {
         System.out.println("Заправить 60л бензина");
+    }
+
+    @Override
+    public void chargeBattery() {
+        Vehicle.super.chargeBattery();
     }
 }
