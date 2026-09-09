1e7bc026aa9abc1f9860e8868c07a64ac6339c23
Kageelee <kirill8102@mail.ru>
2026-09-10 02:59:11 +0500

    9.0. Программируемые объекты для тестирования [#505118 #567044]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,13 +16,28 @@ class StartUITest {
     }
 
     @Test
-    void whenAddItemTwo() {
+    void whenAddItemOne() {
         String[] answers = {"Fix Ethernet"};
         Input input = new MockInput(answers);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix Ethernet");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    void whenAddItemTwo() {
+        String[] answers = {"Fix PC", "Fix Bug"};
+        Input input = new MockInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+        created = tracker.findAll()[1];
+        expected = new Item("Fix Bug");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
 }
